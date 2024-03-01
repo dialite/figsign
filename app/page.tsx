6f7061d9d85063ca06e6370a8012bb629c0aff6e
dialite <raymondolisa01@gmail.com>
2024-03-01 18:01:45 +0100

    Modify tailwind config and global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
+
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1 className="font-xl"></h1>
     </Room>
   );
 }
