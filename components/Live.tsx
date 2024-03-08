7c4a41719c44dbe3a6f5bd29a3faba69a5659678
dialite <raymondolisa01@gmail.com>
2024-03-08 12:10:08 +0100

    Create communication functionality amongst collaborators using live cursors chat

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,11 +1,17 @@
 import { useMyPresence, useOthers } from "@/liveblocks.config";
 import LiveCursors from "./cursor/LiveCursors";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { CursorMode } from "@/types/type";
+import CursorChat from "./cursor/CursorChat";
 
 const Live = () => {
 
   const others = useOthers()
   const [{cursor}, updateMyPresence] = useMyPresence() as any
+
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  })
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault()
@@ -17,7 +23,7 @@ const Live = () => {
   }, [])
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault()
+    setCursorState({mode: CursorMode.Hidden})
 
     updateMyPresence({cursor: null, message: null})
   }, [])
@@ -30,6 +36,35 @@ const Live = () => {
     updateMyPresence({cursor: {x, y}})
   }, [])
 
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if(e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: ""
+        })
+      } else if(e.key === "Escape") {
+        updateMyPresence({message: ""})
+        setCursorState({mode: CursorMode.Hidden})
+      }
+    }
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if(e.key === "/") {
+        e.preventDefault()
+      }
+    }
+
+    window.addEventListener("keyup", onKeyUp)
+    window.addEventListener("keydown", onKeyDown)
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp)
+      window.removeEventListener("keydown", onKeyDown)
+    }
+  }, [updateMyPresence])
+
   return (
     <div 
       onPointerMove={handlePointerMove}
@@ -38,6 +73,16 @@ const Live = () => {
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
       <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+
+      {cursor && (
+        <CursorChat 
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
+
       <LiveCursors others={others}/>
     </div>
   );
