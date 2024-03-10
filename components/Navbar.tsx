f272d9c1657a9960573526e793d0b26b9b6f3a02
dialite <raymondolisa01@gmail.com>
2024-03-10 16:52:27 +0100

    Initialize navbar elements for canvas and install shadcn button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,11 @@ import { memo } from "react";
 
 import { navElements } from "@/constants";
 import { ActiveElement, NavbarProps } from "@/types/type";
-import ActiveUsers from "./users/ActiveUsers";
 
-// import { Button } from "./ui/button";
-// import ShapesMenu from "./ShapesMenu";
-// import ActiveUsers from "./users/ActiveUsers";
-// import { NewThread } from "./comments/NewThread";
+import { Button } from "./ui/button";
+import ShapesMenu from "./ShapesMenu";
+import ActiveUsers from "./users/ActiveUsers";
+import { NewThread } from "./comments/NewThread";
 
 const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps) => {
   const isActive = (value: string | Array<ActiveElement>) =>
@@ -20,6 +19,53 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+
+      <ul className="flex flex-row">
+        {navElements.map((item: ActiveElement | any) => (
+          <li
+            key={item.name}
+            onClick={() => {
+              if (Array.isArray(item.value)) return;
+              handleActiveElement(item);
+            }}
+            className={`group px-2.5 py-5 flex justify-center items-center
+            ${isActive(item.value) ? "bg-primary-green" : "hover:bg-primary-grey-200"}
+            `}
+          >
+            {/* If value is an array means it's a nav element with sub options i.e., dropdown */}
+            {Array.isArray(item.value) ? (
+              <ShapesMenu
+                item={item}
+                activeElement={activeElement}
+                imageInputRef={imageInputRef}
+                handleActiveElement={handleActiveElement}
+                handleImageUpload={handleImageUpload}
+              />
+            ) : item?.value === "comments" ? (
+              // If value is comments, trigger the NewThread component
+              <NewThread>
+                <Button className="relative w-5 h-5 object-contain">
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    fill
+                    className={isActive(item.value) ? "invert" : ""}
+                  />
+                </Button>
+              </NewThread>
+            ) : (
+              <Button className="relative w-5 h-5 object-contain">
+                <Image
+                  src={item.icon}
+                  alt={item.name}
+                  fill
+                  className={isActive(item.value) ? "invert" : ""}
+                />
+              </Button>
+            )}
+          </li>
+        ))}
+      </ul>
 
       <ActiveUsers />
     </nav>
