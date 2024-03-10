5e4912c176a44c6ffa8cd95a9f9955dbd72be88d
dialite <raymondolisa01@gmail.com>
2024-03-10 16:16:35 +0100

    Initialize canvas functionality by making selected shape ref to be rectangle

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,21 @@ const nextConfig = {
                 port: ""
             }
         ]
-    }
+    },
+    
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+
+    webpack: (config) => {
+        config.externals.push({
+          "utf-8-validate": "commonjs utf-8-validate",
+          bufferutil: "commonjs bufferutil",
+          canvas: "commonjs canvas",
+        });
+        // config.infrastructureLogging = { debug: /PackFileCache/ };
+        return config;
+      },
 };
 
 export default nextConfig;
