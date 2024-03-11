2a71892bd39e5b1d388c55d880599e54b58ef4d7
dialite <raymondolisa01@gmail.com>
2024-03-11 10:40:43 +0100

    Create add image functionality to canvas

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 
 export default function Page() {
   const undo = useUndo();
@@ -31,6 +32,7 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null)
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -81,8 +83,18 @@ export default function Page() {
         setActiveElement(defaultNavElement);
         break;
       case "delete":
-        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        handleDelete(fabricRef.current as any,
+        deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break
+      case "image":
+        imageInputRef.current?.click()
+        isDrawing.current = false
+
+        if(fabricRef.current) {
+          fabricRef.current.isDrawingMode = false
+        }
+        break
 
       default:
         break;
@@ -167,6 +179,15 @@ export default function Page() {
       <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {e.stopPropagation()
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage
+          })
+        }}
       />
 
       <section className="flex h-full flex-roll">
