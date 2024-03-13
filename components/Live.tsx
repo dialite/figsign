7f55f67db5ef4d7b57ac1d9fe45b917a1eded933
dialite <raymondolisa01@gmail.com>
2024-03-13 14:35:00 +0100

    Add react comment features functionality

--- components/Live.tsx
+++ components/Live.tsx
@@ -6,6 +6,7 @@ import CursorChat from "./cursor/CursorChat";
 import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
+import { Comments } from "./comments/Comments";
 
 type Props = {
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>
@@ -149,7 +150,7 @@ const Live = ({canvasRef}: Props) => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className="h-[100vh] w-full flex justify-center items-center text-center"
+      className="relative h-full w-full flex flex-1 justify-center items-center"
     >
       <canvas ref={canvasRef} />
 
@@ -177,6 +178,8 @@ const Live = ({canvasRef}: Props) => {
       )}
 
       <LiveCursors others={others} />
+
+      <Comments />
     </div>
   );
 };
