818be26ba908d8871ff181aa4bc1c47c5ced3b73
dialite <raymondolisa01@gmail.com>
2024-03-15 06:37:04 +0100

    Setup free drawing functionality

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import {
   handleCanvasObjectScaling,
   handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
+  handlePathCreated,
   handleResize,
   initializeFabric,
   renderCanvas,
@@ -171,6 +172,12 @@ export default function Page() {
     canvas.on("object:scaling", (options: any) => {
       handleCanvasObjectScaling({
         options, setElementAttributes
+      })
+    })
+
+    canvas.on("path:created", (options: any) => {
+      handlePathCreated({
+        options, syncShapeInStorage
       })
     })
 
